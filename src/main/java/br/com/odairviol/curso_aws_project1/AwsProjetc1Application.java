98efd1c9d3d223f60fa92d37fc160b11ea4cac2b
Exacta <odair.viol@gmail.com>
2021-08-16 16:27:15 -0300

    Commit projeto inicial

---
+++ src/main/java/br/com/odairviol/curso_aws_project1/AwsProjetc1Application.java
@@ -0,0 +1,13 @@
+package br.com.odairviol.curso_aws_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjetc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjetc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/odairviol/curso_aws_project1/AwsProjetc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.odairviol.curso_aws_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjetc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
